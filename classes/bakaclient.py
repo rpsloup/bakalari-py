5a7aeac10be9ce9f4cf5b2390a209b320444192c
rpsloup <rpsloup@proton.me>
2023-03-06 22:50:45 +0100

    Fetching the access token from the API

--- classes/bakaclient.py
+++ classes/bakaclient.py
@@ -1,3 +1,30 @@
+"""Module allowing for the usage of HTTP Requests"""
+import requests
+
+def fetch_api_token(auth_data):
+    """Fetches the access token from the API by entered data"""
+    headers = { 'Content-Type' : 'application/x-www-form-urlencoded' }
+    response = requests.post(
+        f'{auth_data["endpoint"]}/api/login',
+        headers=headers,
+        data=f'client_id=ANDR&grant_type=password&\
+               username={auth_data["username"]}&password={auth_data["password"]}',
+        timeout=10
+    )
+    if response.status_code == 200:
+        data = response.json()
+        print(data['access_token'])
+
 class BakaClient:
-  def __init__(self):
-    pass
+    """Custom wrapper class for accessing the Bakaláři API"""
+
+    def __init__(self):
+        self.auth = None
+
+    def login(self, auth_data):
+        """Handles login by fetching the access token"""
+        self.auth = auth_data
+        fetch_api_token(auth_data)
+
+    def fetch(self, endpoint):
+        """Handles fetching data from the Bakaláři API"""
