f0d3da6213e95e379ef6f1c2b91819152893287d
rpsloup <rpsloup@proton.me>
2023-03-07 11:41:20 +0100

    Adding more modules to the wrapper and allowing for fetching the final marks

--- classes/bakaclient.py
+++ classes/bakaclient.py
@@ -1,8 +1,8 @@
-"""Module allowing for the usage of HTTP Requests"""
+"""Module allowing for the usage of HTTP Requests."""
 import requests
 
 def fetch_api_token(auth_data):
-    """Fetches the access token from the API by entered data"""
+    """Fetches the access token from the API by entered data."""
     try:
         headers = { 'Content-Type' : 'application/x-www-form-urlencoded' }
         response = requests.post(
@@ -21,7 +21,7 @@ def fetch_api_token(auth_data):
         return None
 
 def fetch_from_api(token_data, auth_data, endpoint):
-    """Fetches data from the API via the passed access token"""
+    """Fetches data from the API via the passed access token."""
     try:
         headers = {
             'Content-Type' : 'application/x-www-form-urlencoded',
@@ -41,29 +41,39 @@ def fetch_from_api(token_data, auth_data, endpoint):
         return None
 
 class BakaClient:
-    """Custom wrapper class for accessing the Bakaláři API"""
+    """Custom wrapper class for accessing the Bakaláři API."""
 
     def __init__(self):
         self.auth_data = None
         self.token_data = None
 
     def login(self, auth_data):
-        """Handles login by fetching the access token"""
+        """Handles login by fetching the access token."""
         token_data = fetch_api_token(auth_data)
         self.auth_data = auth_data
         self.token_data = token_data
 
     def fetch(self, endpoint):
-        """Handles fetching data from the Bakaláři API"""
+        """Handles fetching data from the Bakaláři API."""
         data = fetch_from_api(self.token_data, self.auth_data, endpoint)
         return data
 
     def get_subjects(self):
-        """Fetches the subjects from the API"""
+        """Fetches the subjects from the API."""
         data = fetch_from_api(self.token_data, self.auth_data, '/subjects')
         return data['Subjects']
 
     def get_timetable(self):
-        """Fetches the timetable from the API"""
+        """Fetches the timetable from the API."""
         data = fetch_from_api(self.token_data, self.auth_data, '/timetable/actual')
+        return data
+
+    def get_marks(self, final=False):
+        """Fetches the marks from the API."""
+        data = fetch_from_api(self.token_data, self.auth_data, f'/marks{"/final" if final else ""}')
+        return data
+
+    def get_changes(self):
+        """Fetches the changes from the API."""
+        data = fetch_from_api(self.token_data, self.auth_data, '/substitutions')
         return data
